e7c7e69ae6eafd26bd897b99fcf3644a3d857fde
joyounin <3723519@naver.com>
2023-03-09 11:28:09 +0900

    styling #2

---
+++ 2.component/ex02/src/05/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./assets/scss/App.scss";
+
+function App(props) {
+  return (
+    <div id="App">
+        <h1 className={style.Header}>SASS & SCSS</h1>
+    </div>
+  );
+}
+
+export default App;
