fe4ee2bc9c894d96d989498d6b56f8ec98df3a78
joyounin <3723519@naver.com>
2023-03-13 16:29:15 +0900

    emaillist #1

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './assets/css/App.css';
 import Searchbar from './Searchbar';
 import RegisterForm from './RegisterForm';
 import Emaillist from './Emaillist';
-import emails from './assets/json/data.json';
+import data from './assets/json/data.json';
 
 
 function App(props) {
+    const [emails, setEmails] = useState(data);
+    const notifyKeyWordChanged = function(){
+        // keyword가 firstName or lastName or email
+        const newEmails = emails.filter(function(e) {return true})
+    }
+
     return (
         <div id='App' className={'App'}>
            <RegisterForm />          
--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './assets/css/KanbanBoard.css';
-import cards from './assets/json/data.json';
+import data from './assets/json/data.json';
 import CardList from './CardList';
 
 const KanbanBoard = () => {
+    const [cards, setCard] = useState(data);
+
+    const changeTaskDone = function(cardNo, taskNo, done){
+        const cardIndex = cards.findIndex(cardNo, card => card.no === cardNo);
+        const taskIndex = cards[cardIndex].tasks.findIndex(task => task.no === no);
+        console.log(cardNo, taskNo, done);
+        // console.log(index, done);
+        // stateTasks[index].tasks[taskIndex].done = done;
+
+        // setTasks(cards);
+    }
     return (
         <div className={styles.KanbanBoard}>
             <CardList 
                 key={'To Do'}
                 title={'To Do'}
-                cards={cards.filter(card => card.status === 'ToDo')}/>
+                cards={cards.filter(card => card.status === 'ToDo')}
+                callback={changeTaskDone}/>
             <CardList
                 key={'Doing'}
                 title={'Doing'}
-                cards={cards.filter(card => card.status === 'Doing')}/>
+                cards={cards.filter(card => card.status === 'Doing')}
+                callback={changeTaskDone}/>
             <CardList
                 key={'Done'}
                 title={'Done'}
-                cards={cards.filter(card => card.status === 'Done')}/>
+                cards={cards.filter(card => card.status === 'Done')}
+                callback={changeTaskDone}/>
         </div>
     );
 };
