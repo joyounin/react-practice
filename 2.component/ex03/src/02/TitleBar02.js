29c477d50b6976fa93b6e5ca1b97009b5258dfa5
joyounin <3723519@naver.com>
2023-03-13 11:26:58 +0900

    event #2

--- 2.component/ex03/src/02/TitleBar02.js
+++ 2.component/ex03/src/02/TitleBar02.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TitelBar02() {
+    const [no, setNo] = useState(0);
+
+    const onClickHandler = function(){
+        setNo(no + 1);
+        console.log(`TtitleBar02 Clicked: ${no}`)
+    }
 
 
     return (
         <div>
             <h1
-                >Function Handler</h1>
+                onClick={onClickHandler}
+                style={{
+                    cursor: 'pointer'
+                }}>
+                Function Handler
+                <br/>
+                {   
+                    // this.no VS this.stat.no
+                    `${no}`
+                }
+                </h1>
         </div>
     )
 }
