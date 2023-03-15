98c7b11aee41398060c899c504eab9069b569e2d
joyounin <3723519@naver.com>
2023-03-15 11:23:24 +0900

    kanbanboard #1

--- kanbanboard/frontend/src/TaskList.js
+++ kanbanboard/frontend/src/TaskList.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import Task from './Task';
+import styles from './assets/css/TaskList.css';
 
-const TaskList = ({cardNo, tasks, callback}) => {
+const TaskList = ({cardNo, tasks, callbackAddTask}) => {
+
     return (
         <div>
             <ul>
@@ -11,10 +13,18 @@ const TaskList = ({cardNo, tasks, callback}) => {
                                         no={task.no}
                                         cardNo={cardNo}                                        
                                         name={task.name}
-                                        done={task.done}
-                                        callback={callback}/>)
+                                        done={task.done}/>)
                 }
             </ul>
+            <input
+                type='text'
+                placeholder={'태스크 추가'}
+                className={styles.TaskList__add_task}
+                onKeyDown={(e) => {
+                    if(e.key === 'Enter') {
+                        callbackAddTask(e.target.value);        
+                    }
+                }}/>
         </div>
     );
 };
