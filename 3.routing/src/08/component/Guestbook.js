af8d9a38574e9b1a5ecc160fc00ddcc646cbea00
joyounin <3723519@naver.com>
2023-03-16 13:49:17 +0900

    routing #finish

--- 3.routing/src/08/component/Guestbook.js
+++ 3.routing/src/08/component/Guestbook.js
@@ -1,9 +1,6 @@
-import React, {Fragment} from 'react';
-import Header from "../layout/Header";
-import Navigation from "../layout/Navigation";
-import Footer from "../layout/Footer";
+import React from 'react';
+import SiteLayout from "../layout/SiteLayout";
 import styles from '../assets/scss/component/Guestbook.scss';
-import SiteLayout from '../layout/SiteLayout';
 
 export default function Gallery() {
     return (
